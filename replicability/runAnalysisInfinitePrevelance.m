36964c41fbc37660ef7021fa8907b3ab77211828
Roee Gilron <roeegilron@gmail.com>
2016-12-06 19:45:12 -0800

    more changeS

--- replicability/runAnalysisInfinitePrevelance.m
+++ replicability/runAnalysisInfinitePrevelance.m
@@ -1,4 +1,5 @@
 function runAnalysisInfinitePrevelance(varargin)
+maxNumCompThreads(1); 
 cd('..'); 
 addpath(genpath(pwd)); 
 cd('replicability');
@@ -12,7 +13,7 @@ else
     roisuse = varargin{1}; 
 end
 
-numtrialsuse = 3;
+numtrialsuse = 80;
 for r = roisuse% loop on rois
     strtsave = tic;
     cnt = 1;
@@ -21,15 +22,21 @@ for r = roisuse% loop on rois
             startex = tic; 
             [data, labels] = getDataPerSub(params.subuse(s),r,settings,params);
             [data, labels] = trimdataforprev(data,labels,t);
-            params.numshufs = 1; % bcs we don't care about shuffels for now 
-            [pval, ansMat] = run_MultiT2013_On_ROI(data,labels,settings,params); % for ruti
-            dataprev(cnt,1) = ansMat(1); 
-            dataprev(cnt,2) = t; 
-            cnt = cnt + 1; 
-            fprintf('sub %d roi %d numtrials %d computed in %f\n',...
-                params.subuse(s),r, t, toc(startex)); 
+            % makes sure there is enough trials, else , skips this subject with this num of trials
+            if ~isempty(data)
+                params.numshufs = 1; % bcs we don't care about shuffels for now
+                [pval, ansMat] = run_MultiT2013_On_ROI(data,labels,settings,params); % for ruti
+                dataprev(cnt,1) = ansMat(1);
+                dataprev(cnt,2) = t;
+                cnt = cnt + 1;
+                fprintf('sub %d roi %d numtrials %d computed in %f\n',...
+                    params.subuse(s),r, t, toc(startex));
+            end
         end
     end
+    save(fullfile(fldrsve,fnmsave),...
+        'dataprev','numtrialsuse','settings','params');
+
     perc = estimate_Prevelane(dataprev);
     fnmsave = sprintf('roi_%.3d_inf_prev.mat',r);
     fldrsve = settings.resdir_inf_ss_prev;
--- replicability/trimdataforprev.m
+++ replicability/trimdataforprev.m
@@ -3,10 +3,14 @@ idxA = find(labels==1);
 idxB = find(labels==2); 
 idxAshuf = idxA(randperm(length(idxA)));
 idxBshuf = idxB(randperm(length(idxB)));
-
-dataAout = data(idxAshuf(1:trialskeep),:);
-dataBout = data(idxBshuf(1:trialskeep),:);
-
-dataout = [dataAout ; dataBout]; 
-labelsout = [ones(size(dataAout,1),1) ; ones(size(dataBout,1),1).*2];
+if trialskeep >  length(idxAshuf)
+    dataout = []; 
+    labelsout = []; 
+else
+    dataAout = data(idxAshuf(1:trialskeep),:);
+    dataBout = data(idxBshuf(1:trialskeep),:);
+    
+    dataout = [dataAout ; dataBout];
+    labelsout = [ones(size(dataAout,1),1) ; ones(size(dataBout,1),1).*2];
+end
 end
