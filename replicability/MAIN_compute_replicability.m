ad4ae338c9d1ff55b4601c7ffee10890686d45e2
Roee Gilron <roeegilron@gmail.com>
2016-12-06 19:30:20 -0800

    more changeS

--- replicability/MAIN_compute_replicability.m
+++ replicability/MAIN_compute_replicability.m
@@ -2,9 +2,9 @@ function MAIN_compute_replicability()
 %% This an anaotmical analysis. Getting data divided by ROI. 
 
 %% run analysis Ruti 
-runGroup(0) % 1 just write reults, 0 do heavy computation 
+runGroup(1) % 1 just write reults, 0 do heavy computation 
 %% run analysis Estimation / infinite trial length 
-runInfinite(1)  
+runInfinite(0)  
 %% run analysis Alllefeld 
 % runAnalysisAllefeldBoundedPrevelance(settings,params);
 % writeVMP_percents(settings,params); 
--- replicability/runInfinite.m
+++ replicability/runInfinite.m
@@ -5,7 +5,7 @@ if ~justwriteres
         runAnalysisInfinitePrevelance();
     elseif isunix
         % run each roi on a seperate cote
-        roisrun = 1:2;
+        roisrun = 2:3;
         for i = 1:length(roisrun)
             % XXX needs work
             % make sure relative paths make sense
