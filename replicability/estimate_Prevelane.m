7d1600aadb60c5c64866d8aecf4bd9b2b32cee96
roeegilron <roeegilron@gmail.com>
2016-12-13 11:37:01 -0800

    Update estimate prev
    add p2 p3 to output

--- replicability/estimate_Prevelane.m
+++ replicability/estimate_Prevelane.m
@@ -9,14 +9,14 @@ initvals(4) = exp(sigma2);
 initvals(5) = exp(sigma3); 
 initvals(6) = mu;
 
-x = fminsearch(@liklihoodfunc2,initvals);
+[x,p2,p3] = fminsearch(@liklihoodfunc2,initvals);
 xout = 1 /(1+exp((-1)*x(1)));
 perc = xout; 
 
 end
 
 
-function outre =  computeF(x,t,p2,p3,sigma1,sigma2,sigma3, mu)
+function[outre,p2,p3] =  computeF(x,t,p2,p3,sigma1,sigma2,sigma3, mu)
 a2 = sigma2/sqrt(t); 
 a3 = sigma3/sqrt(t); 
 b = sqrt(sigma1^2 + sigma2^2/t+ sigma3^2/t);
@@ -24,15 +24,15 @@ outre = p2 * normpdf(x,0,a2) + p3 * normpdf(x,0,a3) + (1-p2-p3) * normpdf(x,mu,b
 
 end
 
-function lres = liklihoodfunc(p2,p3,sigma1,sigma2,sigma3, mu)
+function [lres,p2,p3] = liklihoodfunc(p2,p3,sigma1,sigma2,sigma3, mu)
 global DATA 
 for i = 1: size(DATA,1)
-    res(i) =  computeF(DATA(i,1),DATA(i,2),p2,p3,sigma1,sigma2,sigma3, mu);
+    [res(i) ,p2out(i),p3out(i)]=  computeF(DATA(i,1),DATA(i,2),p2,p3,sigma1,sigma2,sigma3, mu);
 end
 lres =  sum(log(res));
 end
 
-function result = liklihoodfunc2(initvals)
+function [result,p2,p3] = liklihoodfunc2(initvals)
 p2star      = initvals(1);
 p3star      = initvals(2);
 sigma1star = initvals(3);
@@ -45,7 +45,7 @@ p3      = 1 /(1+exp((-1)*p3star));
 sigma1 = log(sigma1star); 
 sigma2 = log(sigma2star); 
 sigma3 = log(sigma3star); 
-result = liklihoodfunc(p2,p3,sigma1,sigma2,sigma3, mu)*(-1); % bcs maximizing 
+[result,p2,p3] = liklihoodfunc(p2,p3,sigma1,sigma2,sigma3, mu); % bcs maximizing 
 end
 
 function f1(x,t,p2,p3,sigma1,sigma2,sigma3,mu)
