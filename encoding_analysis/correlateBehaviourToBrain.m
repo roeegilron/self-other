e0f243ab9905073195ff7392ec789719de6e8351
Roee Gilron <roeegilron@gmail.com>
2016-12-24 18:11:18 -0800

    many changes....

--- encoding_analysis/correlateBehaviourToBrain.m
+++ encoding_analysis/correlateBehaviourToBrain.m
@@ -8,13 +8,13 @@ end
 % loop on subjects and get brain data for each subject 
 for s = 1:length(behavmats)
     [roiidxs,numofrois] = getROIs(behavmats(s).subnum, settings, params);
-    [data,labels,runslabel,labelStrFromData] = loadData(behavmats(s).subnum,settings, params); 
+    [data,labels,runslabel,labelStrFromData,locations,map] = loadData(behavmats(s).subnum,settings, params); 
     for r = 1:numofrois
         roinum = r; 
         roidata = getDataFromROI(data,labels,runslabel,roiidxs,roinum,settings, params); 
         [corrval(r),corrpval(r)]  = correlateData(roidata,labelStrFromData,labels,runslabel,behavmats(s),settings, params); 
     end
-    writeVMPperSub(corrval,behavmats(s).subnum,settings,params)
+    writeVMPperSub(corrval,behavmats(s).subnum,settings,params,locations,map)
     corrmats(:,s) = corrval'; 
     pvalmats(:,s) = corrpval'; 
 end
