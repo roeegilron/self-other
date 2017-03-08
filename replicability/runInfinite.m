f66b3281b1c9ed9ad051f483f33ea17fe4f4e241
Roee Gilron <roee@RoeeG.local>
2017-03-08 19:16:00 +0200

    more changes to run code

--- replicability/runInfinite.m
+++ replicability/runInfinite.m
@@ -5,9 +5,11 @@ if ~justwriteres
         runAnalysisInfinitePrevelance();
     elseif isunix
         % run each roi on a seperate cote
-        roisrun = [5, 17, 20:20, 26:40,49,76,77,58,59];
+        roisrun = [5, 17, 20:20, 26:40,49,76,77,58,59]; % failures first time 
+        roisrun = [     5    16    17    21    28    29    49    58    59    77];
+
         % all rois is 1:111 - can only run 1 roi / core.
-        roisrun = 81:111;
+%         roisrun = 81:111;
         for i = 1:length(roisrun)
             %runAnalysisInfinitePrevelance(roisrun(i));
             % XXX needs work
