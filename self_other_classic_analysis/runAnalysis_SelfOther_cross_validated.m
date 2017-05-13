5731a01635d9f7378f66d985a35ecb9c58199058
Roee Gilron <roee@udp197241uds.ucsf.edu>
2017-05-13 11:46:11 -0700

    fix issue with file name proble for saving

--- self_other_classic_analysis/runAnalysis_SelfOther_cross_validated.m
+++ self_other_classic_analysis/runAnalysis_SelfOther_cross_validated.m
@@ -37,7 +37,7 @@ for ucv = 1:length(unqruns)
 end
 mask = mapclean; 
 locations = locationsclean;
-fnsmv = sprintf('ND_FFX_s-%s_shufs-%d_cross_validate_newMultit.mat',...
+fnsmv = sprintf('ND_FFX_s-%d_shufs-%d_cross_validate_newMultit.mat',...
     subuse,params.numshufs);
 fldrsv = settings.resdir_ss_prev_cv;
 fnmsvfull = fullfile(fldrsv,fnsmv); 
